4eb5b813b6fe8c90dafddaecbf18826051b9400e
Heather Anderson & Frank Meza <student@epicodus.com>
2015-02-11 15:21:43 -0800

    Add initial files.

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+var romanNumeral = function(basicNumber) {
+
+
+
+
+
+
+
+
+
+
+};
+
+// // ---------- jquery below ------------
+
+// $(document).ready(function() {
+//   $("form#roman-numeral").submit(function(event) {
+//     var basic-number = ($("input#basic-number").val());
+//     var result = romanNumeral(basic-number);
+//
+//   $("._____ ").text(result);
+//
+//   event.preventDefault();
+//
+//   });
+// });
---
+++ spec/specs.js
@@ -0,0 +1,7 @@
+describe ("romanNumeral", function() {
+  it("returns I for the number 1", function() {
+    expect(romanNumeral(1)).to.eql(["I"]);
+  });
+
+
+});
